502b415ce364fcac2dc596dbd29499c005c72bc0
netoudi <netoudi@rocketmail.com>
2015-09-23 00:28:50 -0300

    Registranto os Repositories.

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace CodeDelivery\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            \CodeDelivery\Repositories\CategoryRepository::class,
+            \CodeDelivery\Repositories\CategoryRepositoryEloquent::class
+        );
+
+        $this->app->bind(
+            \CodeDelivery\Repositories\ClientRepository::class,
+            \CodeDelivery\Repositories\ClientRepositoryEloquent::class
+        );
+
+        $this->app->bind(
+            \CodeDelivery\Repositories\OrderItemRepository::class,
+            \CodeDelivery\Repositories\OrderItemRepositoryEloquent::class
+        );
+
+        $this->app->bind(
+            \CodeDelivery\Repositories\OrderRepository::class,
+            \CodeDelivery\Repositories\OrderRepositoryEloquent::class
+        );
+
+        $this->app->bind(
+            \CodeDelivery\Repositories\ProductRepository::class,
+            \CodeDelivery\Repositories\ProductRepositoryEloquent::class
+        );
+
+        $this->app->bind(
+            \CodeDelivery\Repositories\UserRepository::class,
+            \CodeDelivery\Repositories\UserRepositoryEloquent::class
+        );
+    }
+}
