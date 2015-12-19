320c166cfe63ded18a4e72ef4e1335dda658479f
netoudi <netoudi@rocketmail.com>
2015-12-19 13:54:21 -0200

    Criando o seed OAuthClientSeeder.

---
+++ database/seeds/OAuthClientSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OAuthClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('oauth_clients')->insert(
+            [
+                'id' => 'appid01',
+                'secret' => 'secret',
+                'name' => 'Minha App Mobile',
+                'created_at' => new \DateTime,
+                'updated_at' => new \DateTime
+            ]
+        );
+    }
+}
