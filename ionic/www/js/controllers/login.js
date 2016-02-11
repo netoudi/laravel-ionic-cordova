5d26ced960b017f077e2ee8f32ea7def38575167
netoudi <netoudi@rocketmail.com>
2016-02-11 11:19:12 -0200

    Trabalhando com o $q.

--- ionic/www/js/controllers/login.js
+++ ionic/www/js/controllers/login.js
@@ -1,22 +1,34 @@
 angular.module('starter.controllers')
-    .controller('LoginCtrl', ['$scope', 'OAuth', '$ionicPopup', '$state', function ($scope, OAuth, $ionicPopup, $state) {
+    .controller('LoginCtrl', [
 
-        $scope.user = {
-            username: 'user@user.com',
-            password: '123456'
-        };
+        '$scope', 'OAuth', 'OAuthToken', '$ionicPopup', '$state', '$localStorage', 'User',
 
-        $scope.login = function () {
-            OAuth.getAccessToken($scope.user).then(function ($data) {
-                console.log($data);
-                $state.go('client.view-products');
-            }, function ($responseError) {
-                console.log($responseError);
-                $ionicPopup.alert({
-                    title: 'Advertência!',
-                    template: '<div class="text-center">Login e/ou senha inválidos.</div>'
-                });
-            });
-        };
+        function ($scope, OAuth, OAuthToken, $ionicPopup, $state, $localStorage, User) {
 
-    }]);
+            $scope.user = {
+                username: '',
+                password: ''
+            };
+
+            $scope.login = function () {
+                var promise = OAuth.getAccessToken($scope.user);
+
+                promise
+                    .then(function (data) {
+                        return User.authenticated({include: 'client'}).$promise;
+                    })
+                    .then(function (data) {
+                        $localStorage.set('user', data.data);
+                        $state.go('client.view-products');
+                    }, function (responseError) {
+                        $localStorage.set('user', null);
+                        OAuthToken.removeToken();
+                        $ionicPopup.alert({
+                            title: 'Advertência!',
+                            template: '<div class="text-center">Login e/ou senha inválidos.</div>'
+                        });
+                        console.debug(responseError);
+                    });
+            };
+
+        }]);
